418a3e70e436f7890c0e0f85b4fcfc724060acab
Nick Best <nick.best5456@gmail.com>
2020-11-07 08:12:51 -0800

    some small update

--- dynamic_programming/fib_bottom_up/python/fib_bottom_up.py
+++ dynamic_programming/fib_bottom_up/python/fib_bottom_up.py
@@ -7,6 +7,6 @@ def fib_bottom_up(n):
     
     fib = [1, 1]
     for i in range(2, n + 1):
-        fib.append(fib[i -1] + fib[i - 2])
+        fib.append(fib[i - 1] + fib[i - 2])
     
     return fib[n - 1]
